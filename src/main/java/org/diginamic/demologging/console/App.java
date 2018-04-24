211d5aba8ca9d629ae47f57a2b2b2032ca936490
Geraud Tourrilhes <geraud.tourrilhes@gmail.com>
2018-04-24 14:26:25 +0200

    exercice logging

---
+++ src/main/java/org/diginamic/demologging/console/App.java
@@ -0,0 +1,17 @@
+package org.diginamic.demologging.console;
+	
+import org.diginamic.demologging.service.AppService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+	public class App {
+		private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+		public static void main (String[] args)
+		{
+			LOG.info("Bonjour Slf4J !");
+			LOG.info("Implémentation Logback");
+			new AppService().executer("valeur 1");
+		}
+	}
+
